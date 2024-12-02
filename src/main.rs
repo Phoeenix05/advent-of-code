87dda7e4bc6cfd18cdbc85e9fa00aef4862cd6cc
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-12-02 06:39:11 +0200

    working answer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 fn main() {
     let input: &'static str = include_str!("input.txt");
 
-    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input
+    let (left, right): (Vec<u32>, Vec<u32>) = input
         .lines()
         .map(|line| {
             let mut nums = line
@@ -12,14 +14,15 @@ fn main() {
         })
         .unzip();
 
-    left.sort();
-    right.sort();
+    let mut map: HashMap<u32, u32> = HashMap::new();
+    right.iter().for_each(|n| {
+        // why does derefencing the map work???
+        *map.entry(*n).or_insert(0) += 1;
+    });
 
-    let sum: i32 = left
+    let similarity: i32 = left
         .iter()
-        .zip(right.iter())
-        .map(|(l, r)| (*l as i32 - *r as i32).abs())
+        .map(|n| (*n as i32 * *map.get_key_value(n).unwrap_or((&0u32, &0u32)).1 as i32).abs())
         .sum();
-
-    println!("{}", sum);
+    println!("{}", similarity);
 }
